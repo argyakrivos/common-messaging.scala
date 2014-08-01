613f92843161752a9693663aa3d2ce81ff37f50f
Greg Beech <greg@blinkbox.com>
2014-08-01 18:05:47 +0100

    Added test for JSON date/times being UTC

--- src/test/scala/com/blinkbox/books/messaging/JsonEventBodyTests.scala
+++ src/test/scala/com/blinkbox/books/messaging/JsonEventBodyTests.scala
@@ -3,13 +3,14 @@ package com.blinkbox.books.messaging
 import java.io.ByteArrayInputStream
 import java.nio.charset.StandardCharsets
 
+import org.joda.time.{DateTimeZone, DateTime}
 import org.json4s.JsonAST.JString
 import org.json4s.jackson.JsonMethods
 import org.scalatest.FunSuite
 
 object JsonEventBodyTests {
   case class TestEvent1(foo: String)
-  case class TestEvent2(foo: String, bar: Int)
+  case class TestEvent2(bar: DateTime)
 
   implicit object TestEvent1 extends JsonEventBody[TestEvent1] {
     val jsonMediaType = MediaType("application/vnd.blinkbox.books.events.testevent.v1+json")
@@ -36,6 +37,13 @@ class JsonEventBodyTests extends FunSuite {
     assert((json \ "$schema") == JString("events.testevent.v1"))
   }
 
+  test("Serializes DateTime objects to ISO format in UTC") {
+    val now = new DateTime(2014, 7, 26, 11, 34, 2, 293, DateTimeZone.UTC)
+    val body = JsonEventBody(TestEvent2(now))
+    val json = JsonMethods.parse(new ByteArrayInputStream(body.content))
+    assert((json \ "bar") == JString("2014-07-26T11:34:02.293Z"))
+  }
+
   test("Deconstructs a JSON event body when the media type matches") {
     val event = TestEvent1("wibble")
     val body = JsonEventBody(event)
@@ -46,5 +54,12 @@ class JsonEventBodyTests extends FunSuite {
     val event = TestEvent1("wibble")
     val body = JsonEventBody(event)
     assert(JsonEventBody.unapply[TestEvent2](body) == None)
+  }
+
+  test("Deserializes DateTime objects in UTC") {
+    val now = new DateTime(2014, 7, 26, 11, 34, 2, 293, DateTimeZone.UTC)
+    val body = JsonEventBody(TestEvent2(now))
+    val body2 = JsonEventBody.unapply[TestEvent2](body)
+    assert(body2.isDefined && body2.get.bar.getZone == DateTimeZone.UTC)
   }
 }
