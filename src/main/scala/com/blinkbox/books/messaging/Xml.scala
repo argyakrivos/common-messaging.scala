c7e5b296d9d133798608000d213e708d97fc0aa0
Egidijus Jankauskas <egidijus@blinkbox.com>
2014-08-06 18:45:35 +0100

    XML schema validation & XML convenience methods

--- src/main/scala/com/blinkbox/books/messaging/Xml.scala
+++ src/main/scala/com/blinkbox/books/messaging/Xml.scala
@@ -1,19 +1,45 @@
 package com.blinkbox.books.messaging
 
-import java.io.ByteArrayInputStream
 import java.io.InputStream
-import java.text.{ DateFormat, SimpleDateFormat }
-import java.util.Date
 import javax.xml.validation.SchemaFactory
 import javax.xml.transform.stream.StreamSource
 import javax.xml.transform.Source
 import javax.xml.XMLConstants
+
+import org.joda.time.DateTime
+import org.joda.time.format.{DateTimeFormat, ISODateTimeFormat}
+
 import scala.xml.NodeSeq
 
 /**
  * Collection of utility functions for parsing XML messages from platform services.
  */
 object Xml {
+
+  private def schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+
+  /**
+   * The resource for the schema will be looked up on the classpath. The directory
+   * of the main schema will be used as the base directory for the validator, which means
+   * that imported schemas in the same directory will be automatically picked up.
+   *
+   * @param schemaNames A list of schema names to use. Note, that the order of the names matters.
+   * @return A validator for a combined schema
+   */
+  def validatorFor(schemaNames: String*) = {
+    var inputs = Array[InputStream]()
+    try {
+      for (schemaName <- schemaNames) {
+        inputs = inputs :+ getClass.getResourceAsStream(schemaName)
+      }
+      val sources: Array[Source] = inputs.map(new StreamSource(_))
+      schemaFactory.newSchema(sources).newValidator()
+    } finally {
+      inputs.foreach(_.close())
+    }
+  }
+
+  case class XMLParsingException(message: String, cause: Throwable = null) extends Exception(message, cause)
 
   /**
    * Wrapper class that adds convenience methods for getting values to the NodeSeq
@@ -24,26 +50,65 @@ object Xml {
   implicit class NodeSeqWrapper(nodeSeq: NodeSeq) {
 
     /**
-     * Get the value of the one and only direct child with the given path, otherwise throw an exception.
+     * Get the string value of the one and only direct child with the given path, otherwise throw an exception.
      */
-    def value(path: String): String = {
+    def stringValue(path: String): String = {
       val found = nodeSeq \ path
-      if (found.size != 1) throw new IllegalArgumentException(s"Expected a single value for path '$path' on node $nodeSeq, got: $found")
-      else found.text.trim
+      if (found.size == 0) throw XMLParsingException(s"No matching path '$path' found on node '$nodeSeq'")
+      if (found.size > 1) throw XMLParsingException(
+        s"Expected a single value for path '$path' on node '$nodeSeq', got ${found.size}")
+      found.text.trim
     }
 
     /**
-     * Get the value of the a direct child with the given path if it exists, or return None if no such value exists.
+     * Get the string value of a direct child with the given path if it exists, or return None if no such value exists.
      *
      *  Throws an exception if multiple matching values exist.
      */
-    def optionalValue(path: String): Option[String] = {
+    def stringValueOptional(path: String): Option[String] = {
       val found = nodeSeq \ path
-      if (found.size > 1) throw new IllegalArgumentException(s"Expected at most one value for path '$path' on node $nodeSeq, got: $found")
+      if (found.size > 1) throw XMLParsingException(s"Expected at most one value for path '$path' on node '$nodeSeq', got: ${found.size}")
       else if (found.size == 1) Some(found.text.trim)
       else None
     }
 
-  }
+    /**
+     * Get the Int value of a direct child with the given path if it exists, or return None otherwise.
+     */
+    def intValueOptional(path: String): Option[Int] =
+      try {
+        stringValueOptional(path).map(_.toInt)
+      } catch {
+        case ex: Throwable => throw XMLParsingException("Cannot parse integer", ex)
+      }
 
+    /**
+     * Get the Joda DateTime value of the one and only direct child with the given path, otherwise throw an exception.
+     *
+     * The result is in ISO8601 format without millis and converted to UTC
+     */
+    def dateTimeValue(path: String): DateTime = {
+      val value = stringValue(path)
+      try {
+        ISODateTimeFormat.dateTimeNoMillis.withZoneUTC.parseDateTime(value)
+      } catch {
+        case ex: Throwable => throw XMLParsingException(s"Could not parse date from '$value'", ex)
+      }
+    }
+
+    /**
+     * Get the Joda DateTime value of the one and only direct child with the given path, otherwise throw an exception.
+     * Input string is expected to be formatted as "yyyy-MM-dd".
+     *
+     * The result is in ISO8601 format and converted to UTC
+     */
+    def dateValue(path: String): DateTime = {
+      val value = stringValue(path)
+      try {
+        DateTimeFormat.forPattern("yyyy-MM-dd").withZoneUTC.parseDateTime(value)
+      } catch {
+        case ex: Throwable => throw XMLParsingException(s"Could not parse date from '$value'", ex)
+      }
+    }
+  }
 }
--- src/test/scala/com/blinkbox/books/messaging/XmlTest.scala
+++ src/test/scala/com/blinkbox/books/messaging/XmlTest.scala
@@ -1,47 +1,99 @@
 package com.blinkbox.books.messaging
 
+import org.joda.time.DateTimeConstants
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
-import Xml.NodeSeqWrapper
+import com.blinkbox.books.messaging.Xml.{XMLParsingException, NodeSeqWrapper}
 
 @RunWith(classOf[JUnitRunner])
 class XmlTest extends FunSuite {
 
   val root =
     <root>
-      <element>
-        42
-      </element>
+      <element>42</element>
       <nested>
-        <element>
-          42
-        </element>
+        <element>42</element>
       </nested>
+      <repeated>foo</repeated>
+      <repeated>bar</repeated>
+      <date>2013-10-15</date>
+      <timestamp>2013-10-15T13:32:51Z</timestamp>
     </root>
 
   test("Get required value when it exists") {
-    assert(root.value("element") == "42")
-    assert((root \ "nested").value("element") == "42")
+    assert(root.stringValue("element") == "42")
+    assert((root \ "nested").stringValue("element") == "42")
   }
 
   test("Get required value when it doesn't exist") {
-    val ex = intercept[IllegalArgumentException] { root.value("nonExistent") }
-    assert(ex.getMessage.contains("nonExistent")
-      && ex.getMessage.contains("root"))
-    intercept[IllegalArgumentException] { (root \ "nested").value("nonExistent") }
-    assert(ex.getMessage.contains("nonExistent")
-      && ex.getMessage.contains("nested"))
+    val ex1 = intercept[XMLParsingException] { root.stringValue("nonExistent") }
+    assert(ex1.getMessage.startsWith("No matching path 'nonExistent' found"))
+
+    val ex2 = intercept[XMLParsingException] { (root \ "nested").stringValue("nonExistent") }
+    assert(ex2.getMessage.startsWith("No matching path 'nonExistent' found"))
+  }
+
+  test("Get required value when there are more than one element with the same name") {
+    val ex = intercept[XMLParsingException] { root.stringValue("repeated") }
+    assert(ex.getMessage.startsWith("Expected a single value for path 'repeated'"))
   }
 
   test("Get optional value when it exists") {
-    assert(root.optionalValue("element") == Some("42"))
-    assert((root \ "nested").optionalValue("element") == Some("42"))
+    assert(root.stringValueOptional("element") == Some("42"))
+    assert((root \ "nested").stringValueOptional("element") == Some("42"))
   }
 
   test("Get optional value when it doesn't exist") {
-    assert(root.optionalValue("nonExistent") == None)
+    assert(root.stringValueOptional("nonExistent") == None)
   }
 
+  test("Get optional Int value when it exists") {
+    assert(root.intValueOptional("element") == Some(42))
+  }
+
+  test("Get optional Int value when it does not exist") {
+    assert(root.intValueOptional("nonExistent") == None)
+  }
+
+  test("Get optional Int value when it is invalid") {
+    val ex = intercept[XMLParsingException] { root.intValueOptional("date") }
+    assert(ex.cause.isInstanceOf[NumberFormatException])
+  }
+
+  test("Get Joda DateTime from a timestamp") {
+    val t = root.dateTimeValue("timestamp")
+    assert(t.getYear == 2013)
+    assert(t.getMonthOfYear == DateTimeConstants.OCTOBER)
+    assert(t.getDayOfMonth == 15)
+    assert(t.getHourOfDay == 13)
+    assert(t.getMinuteOfHour == 32)
+    assert(t.getSecondOfMinute == 51)
+    assert(t.getMillisOfSecond == 0)
+  }
+
+  test("Get Joda DateTime from invalid timestamp fields") {
+    intercept[XMLParsingException] { <r><ts>foo</ts></r>.dateTimeValue("ts") }
+    intercept[XMLParsingException] { <r><ts>2013-10-15</ts></r>.dateTimeValue("ts") }
+    intercept[XMLParsingException] { <r><ts>2013-10-15</ts></r>.dateTimeValue("ts") }
+    intercept[XMLParsingException] { <r><ts>2013-10-15 13:32:51Z</ts></r>.dateTimeValue("ts") }
+  }
+
+  test("Get Joda DateTime from date field without time") {
+    val t = root.dateValue("date")
+    assert(t.getYear == 2013)
+    assert(t.getMonthOfYear == DateTimeConstants.OCTOBER)
+    assert(t.getDayOfMonth == 15)
+    assert(t.getHourOfDay == 0)
+    assert(t.getMinuteOfDay == 0)
+    assert(t.getSecondOfDay == 0)
+  }
+
+  test("Get Jda DateTime from invalid date field without time") {
+    intercept[XMLParsingException] { <r><date>foo</date></r>.dateValue("date") }
+    intercept[XMLParsingException] { <r><date>13:32:51</date></r>.dateValue("date") }
+    intercept[XMLParsingException] { <r><date>T13:32:51</date></r>.dateValue("date") }
+    intercept[XMLParsingException] { <r><date>2013-10-15T13:32:51Z</date></r>.dateValue("date") }
+  }
 }
